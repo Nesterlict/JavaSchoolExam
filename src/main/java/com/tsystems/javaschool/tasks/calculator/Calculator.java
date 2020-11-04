6579c66f9edc11dbc0389352c1645b62c65520ed
Dmitrii Gusev <dmitrii.gusev2075@gmail.com>
2020-11-04 12:30:18 +0300

    Tasks completed.

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,5 +1,9 @@
 package com.tsystems.javaschool.tasks.calculator;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class Calculator {
 
     /**
@@ -11,8 +15,17 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
-        // TODO: Implement the logic here
-        return "";
+        if (statement == null || statement.isEmpty() || statement.contains(",") || statement.contains("//")) return null;
+        else {
+            ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+            ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("Nashorn");
+            try {
+                String result = scriptEngine.eval(statement)+"";
+                return Double.isInfinite(Double.parseDouble(result)) ? null : result;
+            } catch (ScriptException e) {
+                System.out.println("Statement is not valid. Return null.");
+            }
+        }
+        return null;
     }
-
 }
--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,6 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -13,9 +14,32 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        if(inputNumbers.contains(null)) throw new CannotBuildPyramidException();
+        int h = isPyramid(inputNumbers.size());
+        if (h != 0) {
+            int[][] resArray = new int[h][2 * h - 1];
+            Collections.sort(inputNumbers);
+            int num = 0;
+            for (int i = 0; i < h; i++) {
+                int k = h - i - 1;
+                for (int j = 0; j <= i; j++) {
+                    resArray[i][k] = inputNumbers.get(num);
+                    num += 1;
+                    k += 2;
+                }
+            }
+            return resArray;
+        }
+        throw new CannotBuildPyramidException();
     }
 
-
+    private int isPyramid(int size) {
+        if (size == 0) return 0;
+        for (int i = 1; i <= size / 2 + 1; i++) {
+            if (size == i * (i + 1) / 2) {
+                return i;
+            }
+        }
+        return 0;
+    }
 }
